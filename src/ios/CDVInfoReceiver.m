7ee3457b8a38cfb25ef047a3a6fc9fd5f0e6b495
AndrewLolo <andrewloloal@gmail.com>
2016-05-13 02:26:49 +0300

    [test] - local notifications

--- src/ios/CDVInfoReceiver.m
+++ src/ios/CDVInfoReceiver.m
@@ -5,20 +5,25 @@
 
 - (void)addNotification:(CDVInvokedUrlCommand*)command
 {
+    CDVPluginResult* pluginResult = nil;
     NSString* notificationBody = [command.arguments objectAtIndex:0];
     id num = [command argumentAtIndex:1];
 
-    [self.commandDelegate runInBackground:^{
-        UILocalNotification *notification = [[UILocalNotification alloc] init];
-        notification.timeZone  = [NSTimeZone systemTimeZone];
-        notification.fireDate  = [[NSDate date] dateByAddingTimeInterval:[num floatValue]];
-        notification.alertAction = @"Ok";
-        notification.alertBody = notificationBody;
-        notification.soundName = UILocalNotificationDefaultSoundName;
-        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    if (notificationBody != nil && [notificationBody length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
 
-        [self execCallback:command];
-    }]
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.timeZone  = [NSTimeZone systemTimeZone];
+    notification.fireDate  = [[NSDate date] dateByAddingTimeInterval:[num floatValue]];
+    notification.alertAction = @"Ok";
+    notification.alertBody = notificationBody;
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
