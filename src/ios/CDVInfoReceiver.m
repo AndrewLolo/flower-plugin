8666f085aa30975e1d859e5d3d6e78f27207df18
AndrewLolo <andrewloloal@gmail.com>
2016-05-13 02:13:03 +0300

    [test] - local notifications

--- src/ios/CDVInfoReceiver.m
+++ src/ios/CDVInfoReceiver.m
@@ -17,7 +17,7 @@
 
     UILocalNotification *notification = [[UILocalNotification alloc] init];
     notification.timeZone  = [NSTimeZone systemTimeZone];
-    notification.fireDate  = [[NSDate date] dateByAddingTimeInterval:num];
+    notification.fireDate  = [[NSDate date] dateByAddingTimeInterval:[num floatValue]];
     notification.alertAction = @"Ok";
     notification.alertBody = notificationBody;
     notification.soundName = UILocalNotificationDefaultSoundName;
